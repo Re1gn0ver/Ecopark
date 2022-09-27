97bf583efb8e0687113986ff0ccef1279a0bd594
Re1gn0ver <114510254+Re1gn0ver@users.noreply.github.com>
2022-09-28 00:31:49 +0700

    Delete util directory

--- util/auth.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken")
-
-const createJwtToken = (userID) => {
-  return jwt.sign({ userID }, process.env.JWT_SECRET, {
-    expiresIn: '10h'
-  })
-}
-
-module.exports = { createJwtToken }
