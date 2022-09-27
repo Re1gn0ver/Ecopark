fb93a93637ba62ecb688ce50fcaf82c6e476395d
Re1gn0ver <114510254+Re1gn0ver@users.noreply.github.com>
2022-09-28 00:31:38 +0700

    Delete src directory

--- src/shared/img/index.js
+++
@@ -1,9 +0,0 @@
-const ImageSource = {
-    ConfirmCustomer : require('./confirm_customer.png'),
-    ConfirmBikenum : require('./confirm_bikenum.png'),
-    ConfirmStation : require('./confirm_station.png'),
-    ConfirmTime : require('./confirm_time.png'),
-    ConfirmMoney : require('./confirm_money.png'),
-}
-
-export default ImageSource
